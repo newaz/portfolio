6020df29ce30a2533d73fc11a798974cec35c37a
interglobalmedia <interglobalmedia@gmail.com>
2017-10-03 15:50:17 -0400

    Committing so can merge master in here.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export const Contact = () => (
 	<div className="Contact-content">
-		<p>Some contact content.</p>
+		<div className='contact-social'>
+			<h2>Follow Me</h2>
+			<ul>
+				<li>
+					<a href="">
+						<img src={''} width="" alt="twitter logo" />
+					</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 )
