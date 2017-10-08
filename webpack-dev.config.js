628b2195655a639961c77318370290a65c05f184
interglobalmedia <interglobalmedia@gmail.com>
2017-10-08 15:13:22 -0400

    Added [path] to file-loader name prop in webpack-prod. Removed limit prop for url-loader and 'image-webpack-loader ref in webpack-dev. Changed class to className in Skills.js.

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -49,10 +49,8 @@ module.exports = {
 				test: /\.(pdf|jpg|png|gif|svg|ico)$/,
 				use: [
 					{
-						loader: 'url-loader',
-						options: { limit: 100000 }
+						loader: 'url-loader'
 					},
-					'image-webpack-loader'
 				]
 			},
 		]
