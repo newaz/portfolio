bc99e2e4bab7f94c4957c79517c0cad30fc7d1cc
interglobalmedia <interglobalmedia@gmail.com>
2017-10-08 21:09:55 -0400

    Removed {Component} from Header.js.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import {burgerToggle} from '../lib/navHelpers';
 import {Home} from './Home';
