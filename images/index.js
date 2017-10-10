c0dda33a2449809b8393ff2deb2d42d53af87f5f
interglobalmedia <interglobalmedia@gmail.com>
2017-10-10 14:01:18 -0400

    Re-added images outside src with a new work images directory for my projects gallery page and to be used in project post. Changed names of these images so can easily import them into components. Styled work view and added a couple of global styles, esp related to img tag.

--- images/index.js
+++ images/index.js
@@ -11,3 +11,13 @@ export footerTwitter from './icons/footerTwitter.png';
 export footerGithub from './icons/footerGithub.png';
 export footerGoogle from './icons/footerGoogle.png';
 export footerLinkedin from './icons/footerLinkedin.png';
+
+export bsurreal from './work/bsurreal.jpg';
+export classiscare from './work/classiscare.jpg';
+export jsCssClock from './work/jsCssClock.jpg';
+export localStorage from './work/localStorage.jpg';
+export randomQuoteMachine from './work/randomQuoteMachine.jpg';
+export rockinSynth from './work/rockinSynth.jpg';
+export simpleWeather from './work/simpleWeather.jpg';
+export travelAgencySite from './work/travelAgnecySite.jpg';
+export wikipediaViewer from './work/wikipediaViewer.jpg';
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,137 @@
 import React from 'react';
 
-export const Work = () => {
-	return (
-		<div className="Work-content">
-			<p>Some work content.</p>
-		</div>
-	);
-}
+import localStorage from '../../images/work/localStorage.jpg';
+import simpleWeather from '../../images/work/simpleWeather.jpg';
+import wikipediaViewer from '../../images/work/wikipediaViewer.jpg';
+import rockinSynth from '../../images/work/rockinSynth.jpg';
+import travelAgencySite from '../../images/work/travelAgencySite.jpg';
+import bsurreal from '../../images/work/bsurreal.jpg';
+import jsCssClock from '../../images/work/jsCssClock.jpg';
+import randomQuoteMachine from '../../images/work/randomQuoteMachine.jpg';
+import classiscare from '../../images/work/classiscare.jpg';
+
+
+export const Work = () => (
+	
+	<div className="Work-content">
+		<ul className="project-list">
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={localStorage} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={simpleWeather} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={wikipediaViewer} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={rockinSynth} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={travelAgencySite} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={bsurreal} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={jsCssClock} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={randomQuoteMachine} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+			<li className="project">
+				<ul className="project-tags">
+					<li>
+						<a href="">tag</a>
+					</li>
+				</ul>
+				<a href="">
+					<figure>
+						<img className="responsive-image" src={classiscare} alt="Thumbnail for" />
+						<figcaption class="project-caption">caption</figcaption>
+					</figure>
+				</a>
+			</li>
+		</ul>
+	</div>
+);
