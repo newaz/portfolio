afa155b08b6ff25cc55de1e424e4a6732190420b
interglobalmedia <interglobalmedia@gmail.com>
2017-10-22 07:37:10 -0400

    Added template to index.html. Changed contents of index.js so that only rendering top App.js there. Changed class used in burgerToggle to responsive-menu from .top. Just makes more sense. Also, in Navigation.js, have one responsive-menu ul and one top ul. Much easier to deal with in React than trying to change around the same ul for desktop and mobile. I like it, and it is more efficient time-wise and css wise.

--- src/lib/navHelpers.js
+++ src/lib/navHelpers.js
@@ -1,8 +1,9 @@
-export const burgerToggle = () => {
-	const linksElem = document.querySelector('.topNarrowLinks');
+export const burgerToggle = (e) => {
+	e.preventDefault();
+	const responsivemenu = document.querySelector('.responsive-menu');
 	const lines = document.querySelectorAll('.line');
 	lines[0].classList.toggle('animate0');
 	lines[1].classList.toggle('hide');
 	lines[2].classList.toggle('animate2');
-	linksElem.classList.toggle('show');
+	responsivemenu.classList.toggle('show');
 }
