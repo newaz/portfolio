d7aae47a4c43ed3aded28f97dfcd9299588d557d
interglobalmedia <interglobalmedia@gmail.com>
2017-10-23 19:54:18 -0400

    Removed dashes from paths.

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -12,16 +12,16 @@ export const Articles = () => {
 			<div className="Articles-content">
 				<h2>articles</h2>
 				<ul className="articles-nav">
-					<CustomLink to='/jest-react-mocks' activeClassName='active' label='jest, react, and mocks'/>
-					<CustomLink to='/flexbox-sticky-footer-react' activeClassName='active' label='flexbox, sticky footer, and react'/>
-					<CustomLink to='/importing-images-react' activeClassName='active' label='importing images in react'/>
-					<CustomLink to='/my-first-presentation' activeClassName='active' label='my first presentation'/>
+					<CustomLink to='/jestreactmocks' activeClassName='active' label='jest, react, and mocks'/>
+					<CustomLink to='/flexboxstickyfooterreact' activeClassName='active' label='flexbox, sticky footer, and react'/>
+					<CustomLink to='/importingimagesreact' activeClassName='active' label='importing images in react'/>
+					<CustomLink to='/myfirstpresentation' activeClassName='active' label='my first presentation'/>
 				</ul>
 				<Switch>
-					<Route path='/jest-react-mocks' component={JestReactMocks}/>
-					<Route path='/flexbox-sticky-footer-react' component={FlexboxStickyFooterReact}/>
-					<Route path='/importing-images-react' component={ImportingImagesReact}/>
-					<Route path='/my-first-presentation' component={FirstPresentation}/>
+					<Route path='/jestreactmocks' component={JestReactMocks}/>
+					<Route path='/flexboxstickyfooterreact' component={FlexboxStickyFooterReact}/>
+					<Route path='/importingimagesreact' component={ImportingImagesReact}/>
+					<Route path='/myfirstpresentation' component={FirstPresentation}/>
 				</Switch>
 			</div>
 		</HashRouter>
--- src/components/Work.js
+++ src/components/Work.js
@@ -16,24 +16,24 @@ export const Work = () => {
 			<div className="Work-content">
 				<h2>projects</h2>
 				<ul className="work-nav">
-					<CustomLink to='/local-storage' activeClassName='active' label='local storage'/>
-					<CustomLink to='/rockin-synth' activeClassName='active' label='rockin synth'/>
-					<CustomLink to='/js-css-clock' activeClassName='active'  label='js css clock'/>
-					<CustomLink to='/simple-weather' activeClassName='active'  label='simple weather'/>
-					<CustomLink to='/travel-agency-site' activeClassName='active' label='travel agency site'/>
-					<CustomLink to='/random-quote-machine' activeClassName='active' label='random quote machine'/>
-					<CustomLink to='/wikipedia-viewer' activeClassName='active' label='wikipedia viewer'/>
+					<CustomLink to='/localstorage' activeClassName='active' label='local storage'/>
+					<CustomLink to='/rockinsynth' activeClassName='active' label='rockin synth'/>
+					<CustomLink to='/jscssclock' activeClassName='active'  label='js css clock'/>
+					<CustomLink to='/simpleweather' activeClassName='active'  label='simple weather'/>
+					<CustomLink to='/travelagencysite' activeClassName='active' label='travel agency site'/>
+					<CustomLink to='/randomquotemachine' activeClassName='active' label='random quote machine'/>
+					<CustomLink to='/wikipediaviewer' activeClassName='active' label='wikipedia viewer'/>
 					<CustomLink to='/bsurreal' activeClassName='active' label='bsurreal'/>
 					<CustomLink to='/classiscare' activeClassName='active' label='classiscare'/>
 				</ul>
 				<Switch>
-					<Route path='/local-storage' component={LocalStorage}/>
-					<Route path='/rockin-synth' component={RockinSynth}/>
-					<Route path='/js-css-clock' component={JsCssClock}/>
-					<Route path='/simple-weather' component={SimpleWeather}/>
-					<Route path='/travel-agency-site' component={TravelAgencySite}/>
-					<Route path='/random-quote-machine' component={RandomQuoteMachine}/>
-					<Route path='/wikipedia-viewer' component={WikipediaViewer}/>
+					<Route path='/localstorage' component={LocalStorage}/>
+					<Route path='/rockinsynth' component={RockinSynth}/>
+					<Route path='/jscssclock' component={JsCssClock}/>
+					<Route path='/simpleweather' component={SimpleWeather}/>
+					<Route path='/travelagencysite' component={TravelAgencySite}/>
+					<Route path='/randomquotemachine' component={RandomQuoteMachine}/>
+					<Route path='/wikipediaviewer' component={WikipediaViewer}/>
 					<Route path='/bsurreal' component={BSurreal}/>
 					<Route path='/classiscare' component={Classiscare}/>
 				</Switch>
