bcc62690c9eeb0a08382339de67d635e65699b71
interglobalmedia <interglobalmedia@gmail.com>
2017-12-08 01:26:21 -0500

    Made custom links more readable.

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -13,18 +13,18 @@ export const Articles = () => {
 			<div className="Articles-content">
 				<h2>articles</h2>
 				<ul className="articles-nav">
-					<CustomLink to='/jestreactmocks' activeClassName='active' label='jest, react, and mocks'/>
-					<CustomLink to='/flexboxstickyfooterreact' activeClassName='active' label='flexbox, sticky footer, and react'/>
-					<CustomLink to='/importingimagesreact' activeClassName='active' label='importing images in react'/>
-					<CustomLink to='/myfirstpresentation' activeClassName='active' label='my first presentation'/>
-					<CustomLink to='/ghpagesgitsubtree' activeClassName='active' label='deploying to gh pages with git subtree'/>
+					<CustomLink to='/jest-react-mocks' activeClassName='active' label='jest, react, and mocks'/>
+					<CustomLink to='/flexbox-sticky-footer-react' activeClassName='active' label='flexbox, sticky footer, and react'/>
+					<CustomLink to='/importing-images-react' activeClassName='active' label='importing images in react'/>
+					<CustomLink to='/my-first-presentation' activeClassName='active' label='my first presentation'/>
+					<CustomLink to='/gh-pages-git-subtree' activeClassName='active' label='deploying to gh pages with git subtree'/>
 				</ul>
 				<Switch>
-					<Route path='/jestreactmocks' component={JestReactMocks}/>
-					<Route path='/flexboxstickyfooterreact' component={FlexboxStickyFooterReact}/>
-					<Route path='/importingimagesreact' component={ImportingImagesReact}/>
-					<Route path='/myfirstpresentation' component={FirstPresentation}/>
-					<Route path='/ghpagesgitsubtree' component={GhPagesGitSubtree}/>
+					<Route path='/jest-react-mocks' component={JestReactMocks}/>
+					<Route path='/flexbox-sticky-footer-react' component={FlexboxStickyFooterReact}/>
+					<Route path='/importing-images-react' component={ImportingImagesReact}/>
+					<Route path='/my-first-presentation' component={FirstPresentation}/>
+					<Route path='/gh-pages-git-subtree' component={GhPagesGitSubtree}/>
 				</Switch>
 			</div>
 		</HashRouter>
